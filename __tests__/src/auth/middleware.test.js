0b7125c10207c2f668ab31e9894baa5d06a2d99d
ColoSRJones <colosrjones@gmail.com>
2019-09-30 20:51:03 -0500

    that is tough one and find errors many tries

--- __tests__/src/auth/middleware.test.js
+++ __tests__/src/auth/middleware.test.js
@@ -1,34 +1,39 @@
 'use strict';
 
-require('../../supergoose.js');
+const supergoose = require('../../supergoose.js');
 const auth = require('../../../src/auth/middleware.js');
-const User = require('../../../src/auth/users-model.js');
+const Users = require('../../../src/auth/users-model.js');
 
 let users = {
-  admin: {username: 'admin', password: 'password', role: 'admin'},
-  editor: {username: 'editor', password: 'password', role: 'editor'},
-  user: {username: 'user', password: 'password', role: 'user'},
+  admin: { username: 'admin', password: 'password', role: 'admin' },
+  editor: { username: 'editor', password: 'password', role: 'editor' },
+  user: { username: 'user', password: 'password', role: 'user' },
 };
 
-beforeAll(() => {
-  return Promise.all(
-    Object.values(users).map(user => new User(user).save())
-  );
+beforeAll(async (done) => {
+  await supergoose.startDB();
+  const adminUser = await new Users(users.admin).save();
+  const editorUser = await new Users(users.editor).save();
+  const userUser = await new Users(users.user).save();
+  done();
 });
 
+afterAll(supergoose.stopDB);
+
 describe('Auth Middleware', () => {
-  
   // admin:password: YWRtaW46cGFzc3dvcmQ=
   // admin:foo: YWRtaW46Zm9v
-  
-  let errorObject = {"message": "Invalid User ID/Password", "status": 401, "statusMessage": "Unauthorized"};
-  
+
+  let errorObject = {
+    message: 'Invalid User ID/Password',
+    status: 401,
+    statusMessage: 'Unauthorized',
+  };
+
   describe('user authentication', () => {
-    
     let cachedToken;
 
     it('fails a login for a user (admin) with the incorrect basic credentials', () => {
-
       let req = {
         headers: {
           authorization: 'Basic YWRtaW46Zm9v',
@@ -38,15 +43,12 @@ describe('Auth Middleware', () => {
       let next = jest.fn();
       let middleware = auth;
 
-      return middleware(req, res, next)
-        .then(() => {
-          expect(next).toHaveBeenCalledWith(errorObject);
-        });
-
+      return middleware(req, res, next).then(() => {
+        expect(next).toHaveBeenCalledWith(errorObject);
+      });
     }); // it()
 
     it('logs in an admin user with the right credentials', () => {
-
       let req = {
         headers: {
           authorization: 'Basic YWRtaW46cGFzc3dvcmQ=',
@@ -56,14 +58,10 @@ describe('Auth Middleware', () => {
       let next = jest.fn();
       let middleware = auth;
 
-      return middleware(req,res,next)
-        .then( () => {
-          cachedToken = req.token;
-          expect(next).toHaveBeenCalledWith();
-        });
-
+      return middleware(req, res, next).then(() => {
+        cachedToken = req.token;
+        expect(next).toHaveBeenCalledWith();
+      });
     }); // it()
-    
   });
-
 });
--- src/auth/router.js
+++ src/auth/router.js
@@ -8,17 +8,19 @@ const auth = require('./middleware.js');
 
 authRouter.post('/signup', (req, res, next) => {
   let user = new User(req.body);
-  user.save()
-    .then( (user) => {
+  user
+    .save()
+    .then((user) => {
       req.token = user.generateToken();
       req.user = user;
       res.set('token', req.token);
       res.cookie('auth', req.token);
       res.send(req.token);
-    }).catch(next);
+    })
+    .catch(next);
 });
 
-authRouter.get('/signin', auth, (req, res, next) => {
+authRouter.post('/signin', auth, (req, res, next) => {
   res.cookie('auth', req.token);
   res.send(req.token);
 });
